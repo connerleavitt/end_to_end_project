0ea53c64c026625ad6cea8098d30389bd9bd11bd
= <ir3456@byu.edu>
2020-09-23 10:59:55 -0600

    updated imports to work properly

--- database/sql_app/database.py
+++ database/sql_app/database.py
@@ -2,13 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-database_url = "sqlite:///./tweets.db"
+database_url = "sqlite:///./database/tweets.db"
 # database_url = "postgresql://user:password@postgresserver/db"
 
 # connect_args only needed for SQLite
-engine = create_engine(
-    database_url, connect_args={"check_same_thread": False}
-)
+engine = create_engine(database_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
