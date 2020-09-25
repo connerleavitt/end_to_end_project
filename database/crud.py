d5c2e7dba19c56d5e18008792270e6f169f12aeb
= <ir3456@byu.edu>
2020-09-25 11:10:33 -0600

    fixing crud.py spacing

--- database/crud.py
+++ database/crud.py
@@ -1,20 +1,22 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 
 
-def read_tweet(db: Session, tweet_id: int):
+def read_tweet(db: Session, tweet_id: int) -> models.Tweet:
     return db.query(models.Tweet).filter(models.Tweet.id == tweet_id).first()
 
 
-def read_tweets(db: Session, skip: int = 0, limit: int = 100):
+def read_tweets(db: Session, skip: int = 0, limit: int = 100) -> List[models.Tweet]:
     return db.query(models.Tweet).offset(skip).limit(limit).all()
 
 
 def create_tweet(db: Session, tweet: schemas.TweetCreate) -> models.Tweet:
-    db_tweet = models.Tweet(text=tweet.text,
-                            favorites=tweet.favorites,
-                            is_retweet=tweet.is_retweet)
+    db_tweet = models.Tweet(
+        text=tweet.text, favorites=tweet.favorites, is_retweet=tweet.is_retweet
+    )
     db.add(db_tweet)
     db.commit()
     db.refresh(db_tweet)
